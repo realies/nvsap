2080f6fe9e7b674da5b37e1af7fc9f056401457e
realies <github@reali.es>
2017-11-25 12:07:02 +0200

    version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nvsap
+
+NVIDIA's SHIELD service requires users to pair devices by filling out a randomly generated PIN in a textfield prompt.
+
+![SHIELD Auth Prompt](https://i.snag.gy/CM8iG1.jpg)
+
+To ease pairing with new remote devices, this software serves as lighweight HTTP server capable of populating a PIN on demand remotely by accepting GET requests on `http://+:47980/auth/1234` where `1234` is a 4 digit PIN. Upon receiving a GET request, the software checks for an existing SHIELD auth prompt, populates the PIN, clicks the `Connect` button and responds with `true` or `false` depending on the success of the operations. Currently there is no way of validating pairing requests and it is expected that a client attempting to pair will receive a pairing status response from the SHIELD server.
+
+![nvsap](https://i.snag.gy/Ekdf7P.jpg)
---
+++ nvsap/Program.cs
@@ -0,0 +1,87 @@
+﻿using AutoIt;
+using Nancy;
+using Nancy.Hosting.Self;
+using System;
+
+namespace nvsap
+{
+    public class SimpleModule : NancyModule
+    {
+        public SimpleModule()
+        {
+            Get["/auth/{code}"] = parameters =>
+            {
+                Program.Log($"Auth request received from {this.Request.UserHostAddress}");
+                return AuthenticateShield(parameters.code) ? "true" : "false";
+            };
+        }
+
+        private bool AuthenticateShield(string code)
+        {
+            if (Program.Busy)
+                return false;
+            try
+            {
+                Program.Busy = true;
+                string targetWindowClass = "[CLASS:#32770]";
+                string targetWindowText = "SHIELD is requesting to connect";
+                if (AutoItX.WinWait(targetWindowClass, targetWindowText, 1) == 0)
+                {
+                    Program.Log("Warning: Auth window does not exist");
+                    return false;
+                }
+                AutoItX.ControlSetText(targetWindowClass, "", "[CLASS:Edit; INSTANCE:1]", code);
+                AutoItX.ControlClick(targetWindowClass, "", "[CLASS:Button; INSTANCE:1]");
+                Program.Log("Auth code send");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Program.Log($"Authentication error: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                Program.Busy = false;
+            }
+        }
+    }
+
+    class Program
+    {
+        public static bool Busy = false;
+        private string url = "http://localhost";
+        private int port = 47980;
+        private NancyHost nancy;
+
+        public Program()
+        {
+            var configuration = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true },
+                RewriteLocalhost = true
+            };
+            var uri = new Uri($"{url}:{port}/");
+            nancy = new NancyHost(configuration, uri);
+        }
+
+        private void Start()
+        {
+            nancy.Start();
+            Log($"Listening on port {port}");
+            Console.ReadKey();
+            nancy.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Start();
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+    }
+}
